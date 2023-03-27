8c60b68b4c39aa79a2bb933a63babdf94a41be03
Ben White <contact@bennjii.dev>
2023-03-27 12:48:11 +1100

    mod: execution agent parentized

--- src/lang/lang.rs
+++ src/lang/lang.rs
@@ -1,12 +1,12 @@
-use std::process::ExitStatus;
+use std::{process::{ExitStatus, Child}, io::{Write, BufReader, BufRead}, thread};
 
-use crate::exec::Executor;
+use crate::exec::{Executor, TerminalStreamType, TerminalStream};
 use phf::{Map, phf_map};
 use serde::{Serialize, Deserialize};
 use crate::lang;
 use tokio::sync::MutexGuard;
 
-type LanguageExecutor = fn(MutexGuard<Executor>) -> Result<ExitStatus, RuntimeError>;
+type LanguageExecutor = fn(&MutexGuard<Executor>) -> Result<Child, RuntimeError>;
 
 static LANGUAGES: Map<&'static str, LanguageExecutor> = phf_map! {
     "python" => lang::python::run,
@@ -53,7 +53,67 @@ impl Languages {
     pub fn run(exec: MutexGuard<Executor>) -> Result<ExitStatus, RuntimeError> {
         match LANGUAGES.get(Self::to_string(&exec.language)) {
             Some(executor) => {
-                executor(exec)
+                let mut execution: Child = match executor(&exec) {
+                    Ok(val) => val,
+                    Err(err) => return Err(err),
+                };
+                
+                let input_vec = exec.terminal_feed.std_cin.iter().map(| v | {
+                    match v.terminal_type {
+                        TerminalStreamType::StandardInput => v.value.as_str(),
+                        _ => ""
+                    }
+                }).collect::<Vec<&str>>().join("\n");
+            
+                let mut input = execution.stdin.take().unwrap();
+                input.write_all(input_vec.as_bytes()).unwrap();
+            
+                let child_stdout = execution
+                    .stdout
+                    .take()
+                    .expect("Internal error, could not take stdout");
+                let child_stderr = execution
+                    .stderr
+                    .take()
+                    .expect("Internal error, could not take stderr");
+            
+                let sender = exec.broadcast.0.clone();
+                let sender2 = exec.broadcast.0.clone();
+            
+                let stdout_thread = thread::spawn(move || {
+                    let stdout_lines = BufReader::new(child_stdout).lines();
+                    for line in stdout_lines {
+                        let line = line.unwrap();
+                        println!("[OKA:OUTPUT]: {}", line);
+            
+                        match sender.send(TerminalStream::new(TerminalStreamType::StandardOutput, line)) {
+                            Ok(val) => println!("[TERM]: Sent output size {}", val),
+                            Err(err) => println!("[TERM]: Failed to send output {:?}", err),
+                        }
+                    }
+                });
+            
+                let stderr_thread = thread::spawn(move || {
+                    let stderr_lines = BufReader::new(child_stderr).lines();
+                    for line in stderr_lines {
+                        let line = line.unwrap();
+                        println!("[ERR:OUTPUT]: {}", line);
+            
+                        match sender2.send(TerminalStream::new(TerminalStreamType::StandardError, line)) {
+                            Ok(val) => println!("[TERM]: Sent output size {}", val),
+                            Err(err) => println!("[TERM]: Failed to send output {:?}", err),
+                        }
+                    }
+                });
+            
+                let status = execution
+                    .wait()
+                    .expect("Internal error, failed to wait on child");
+            
+                stdout_thread.join().unwrap();
+                stderr_thread.join().unwrap();
+            
+                Ok(status)
             }
             None => {
                 Err(RuntimeError::NoExecutor)
--- src/lang/python.rs
+++ src/lang/python.rs
@@ -1,13 +1,9 @@
-use crate::exec::{Executor, TerminalStreamType};
-use std::process::{Command, Stdio, ExitStatus};
-use crate::exec::TerminalStream;
-use std::io::BufReader;
+use crate::exec::{Executor};
+use std::process::{Command, Stdio, Child};
 use crate::lang::RuntimeError;
 use tokio::sync::MutexGuard;
-use std::thread;
-use std::io::{Write, BufRead};
 
-pub fn run(exec: MutexGuard<Executor>) -> Result<ExitStatus, RuntimeError> {
+pub fn run(exec: &MutexGuard<Executor>) -> Result<Child, RuntimeError> {
     // Create File and Fill
     let file_dir = format!("{}", exec.allocated_dir);
     let file_contents: String = exec.src_file.clone();
@@ -21,7 +17,7 @@ pub fn run(exec: MutexGuard<Executor>) -> Result<ExitStatus, RuntimeError> {
     new_args.insert(0, format!("{}/{}.py", file_dir, exec.id));
 
     // Execute File
-    let mut execution = Command::new("python3")
+    let execution = Command::new("python3")
         .args(new_args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -29,60 +25,5 @@ pub fn run(exec: MutexGuard<Executor>) -> Result<ExitStatus, RuntimeError> {
         .spawn()
         .unwrap();
 
-    let input_vec = exec.terminal_feed.std_cin.iter().map(| v | {
-        match v.terminal_type {
-            TerminalStreamType::StandardInput => v.value.as_str(),
-            _ => ""
-        }
-    }).collect::<Vec<&str>>().join("\n");
-
-    let mut input = execution.stdin.take().unwrap();
-    input.write_all(input_vec.as_bytes()).unwrap();
-
-    let child_stdout = execution
-        .stdout
-        .take()
-        .expect("Internal error, could not take stdout");
-    let child_stderr = execution
-        .stderr
-        .take()
-        .expect("Internal error, could not take stderr");
-
-    let sender = exec.broadcast.0.clone();
-    let sender2 = exec.broadcast.0.clone();
-
-    let stdout_thread = thread::spawn(move || {
-        let stdout_lines = BufReader::new(child_stdout).lines();
-        for line in stdout_lines {
-            let line = line.unwrap();
-            println!("[OKA:OUTPUT]: {}", line);
-
-            match sender.send(TerminalStream::new(TerminalStreamType::StandardOutput, line)) {
-                Ok(val) => println!("[TERM]: Sent output size {}", val),
-                Err(err) => println!("[TERM]: Failed to send output {:?}", err),
-            }
-        }
-    });
-
-    let stderr_thread = thread::spawn(move || {
-        let stderr_lines = BufReader::new(child_stderr).lines();
-        for line in stderr_lines {
-            let line = line.unwrap();
-            println!("[ERR:OUTPUT]: {}", line);
-
-            match sender2.send(TerminalStream::new(TerminalStreamType::StandardError, line)) {
-                Ok(val) => println!("[TERM]: Sent output size {}", val),
-                Err(err) => println!("[TERM]: Failed to send output {:?}", err),
-            }
-        }
-    });
-
-    let status = execution
-        .wait()
-        .expect("Internal error, failed to wait on child");
-
-    stdout_thread.join().unwrap();
-    stderr_thread.join().unwrap();
-
-    Ok(status)
+    Ok(execution)
 }
